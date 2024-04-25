335de36963a89ed8a7b09f740c42191f7fe2e4cd
Neek <46835722+neek-coder@users.noreply.github.com>
2024-04-25 10:50:46 +0200

    Weird init, restart

--- danox/bin/danox.dart
+++
@@ -1,6 +0,0 @@
-import 'package:danox/danox.dart';
-
-//TODO: Push everything to git
-void main(List<String> arguments) {
-  Danox.main(arguments);
-}
--- danox/test/danox_test.dart
+++
@@ -1,8 +0,0 @@
-// import 'package:danox/danox.dart';
-// import 'package:test/test.dart';
-
-// void main() {
-//   test('calculate', () {
-//     expect(calculate(), 42);
-//   });
-// }
